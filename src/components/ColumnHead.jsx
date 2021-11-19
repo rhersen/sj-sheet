2fe9085fabf48e8b6f3fa98c43bc46e1e05300b0
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-11-19 10:50:36 +0100

    show full ProductInformation

--- src/components/ColumnHead.jsx
+++ src/components/ColumnHead.jsx
@@ -9,22 +9,11 @@ export default function ColumnHead({ announcements, id }) {
   );
   return (
     <>
-      <span className={styles.prod}>{product(found)}</span>
-      <span >{_.map(_.get(found, "ToLocation"), "LocationName")}</span>
-      <span >{id}</span>
+      <span className={styles.prod}>
+        {_.get(found, "ProductInformation.0")}
+      </span>
+      <span>{_.map(_.get(found, "ToLocation"), "LocationName")}</span>
+      <span>{id}</span>
     </>
   );
-}
-
-function product(found) {
-  let match;
-  const s1 = _.get(found, "ProductInformation.0");
-  match = /^SJ (.*)$/.exec(s1);
-  const s2 = match ? match[1] : s1;
-  match = /^(.stg).ta(p)endel$/.exec(s2);
-  const s3 = match ? match[1] : s2;
-  match = /^(.+)t.ge?n?$/.exec(s3);
-  const s4 = match ? match[1] : s3;
-  match = /^(.+)t.gen (.+)$/.exec(s4);
-  return match ? match[1] + match[2] : s4;
 }
